d8a10873b2db36ac668c7362489d75fdee0c08e5
btrv <18715467+btrv@users.noreply.github.com>
2022-08-18 13:36:10 +0500

    #1
    Scene & Player

---
+++ Player.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+var score : int = 0
+var speed : int = 1
+var jumpForce : int = 600
+var gravity : int = 400
+var velocity : Vector2 = Vector2()
+var sprite : Sprite = $Sprite
